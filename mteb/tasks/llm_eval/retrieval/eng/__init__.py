576e6b99f5303c0f9bd0f312c2f0f003f7ca5de9
Adnan El Assadi <aassadi22@ku.edu.tr>
2026-03-07 17:25:18 -0500

    fix init

--- mteb/tasks/llm_eval/retrieval/eng/__init__.py
+++ mteb/tasks/llm_eval/retrieval/eng/__init__.py
@@ -1,11 +1,25 @@
-from .llm_arguana_retrieval_128k import *
-from .llm_fever_retrieval_128k import *
-from .llm_fiqa_retrieval_128k import *
-from .llm_fiqa_retrieval_32k import *
-from .llm_hotpotqa_retrieval_128k import *
-from .llm_hotpotqa_retrieval_32k import *
-from .llm_msmarco_retrieval_128k import *
-from .llm_nq_retrieval_128k import *
-from .llm_nq_retrieval_32k import *
-from .llm_quora_retrieval_128k import *
-from .llm_scifact_retrieval_128k import *
+from .llm_arguana_retrieval_128k import LLMArguAnaRetrieval128k
+from .llm_fever_retrieval_128k import LLMFEVERRetrieval128k
+from .llm_fiqa_retrieval_128k import LLMFiQARetrieval128k
+from .llm_fiqa_retrieval_32k import LLMFiQARetrieval32k
+from .llm_hotpotqa_retrieval_128k import LLMHotpotQARetrieval128k
+from .llm_hotpotqa_retrieval_32k import LLMHotpotQARetrieval32k
+from .llm_msmarco_retrieval_128k import LLMMSMARCORetrieval128k
+from .llm_nq_retrieval_128k import LLMNQRetrieval128k
+from .llm_nq_retrieval_32k import LLMNQRetrieval32k
+from .llm_quora_retrieval_128k import LLMQuoraRetrieval128k
+from .llm_scifact_retrieval_128k import LLMSciFactRetrieval128k
+
+__all__ = [
+    "LLMArguAnaRetrieval128k",
+    "LLMFEVERRetrieval128k",
+    "LLMFiQARetrieval128k",
+    "LLMFiQARetrieval32k",
+    "LLMHotpotQARetrieval128k",
+    "LLMHotpotQARetrieval32k",
+    "LLMMSMARCORetrieval128k",
+    "LLMNQRetrieval128k",
+    "LLMNQRetrieval32k",
+    "LLMQuoraRetrieval128k",
+    "LLMSciFactRetrieval128k",
+]
